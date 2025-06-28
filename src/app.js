fd8da9b498b24d117d7b015dd4728b807a05eb50
GuilhermePain <guizaorocha543@gmail.com>
2025-06-27 22:59:33 -0300

    feat: adicionando configuração de CORS na api.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import { configDotenv } from 'dotenv';
 import express, { json } from 'express';
 import connectDatabase from './config/database.config.js';
+import routes from './routes/index.js';
+import cors from 'cors';
+
 const app = express();
 const port = process.env.PORT;
-import routes from './routes/index.js';
 
 configDotenv();
+
+app.use(cors({
+  origin: process.env.ORIGIN_CLIENT_URL,
+  credentials: true
+}));
 
 app.use(json());
 
